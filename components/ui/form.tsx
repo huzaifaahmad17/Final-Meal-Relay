29159da1e935e7c1d3167783aa136051db9271d7
huzaifaahmad17 <huzaifa.mfp@gmail.com>
2026-09-25 09:16:11 +0530

    Export form-related components from form.tsx

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -167,3 +167,13 @@ const FormMessage = React.forwardRef<
 });
 FormMessage.displayName = 'FormMessage';
 
+export {
+  useFormField,
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+};
